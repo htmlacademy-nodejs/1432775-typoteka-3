185b36d119b5e84caf387e571c21eb0761cd47a7
IBars <ivanbarsukov7@gmail.com>
2021-10-07 15:07:21 +0300

    templates

--- src/express/express.js
+++ src/express/express.js
@@ -1,8 +1,9 @@
 "use strict";
 
 const express = require(`express`);
+const path = require(`path`);
 
-const {FRONT_PORT} = require(`../const`);
+const {FRONT_PORT, StatusCode} = require(`../const`);
 const {
   mainRouter,
   registerRouter,
@@ -22,5 +23,17 @@ app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
 app.use(`/search`, searchRouter);
 app.use(`/categories`, categoriesRouter);
+
+app.use(express.static(path.resolve(__dirname, `public`)));
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
+app.use((req, res) => res.render(`404`, {status: 404, url: req.url}));
+app.use((err, _, res) =>
+  res.render(`500`, {
+    status: err.status || StatusCode.INTERNAL_SERVER_ERROR,
+    error: err,
+  })
+);
 
 app.listen(FRONT_PORT);
