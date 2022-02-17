06ad3a2dcf04937c2e80fafea4b3b27fdf205bfe
Ibarsuk <ivanbarsukov7@gmail.com>
2022-02-17 12:43:03 +0300

    filldb file now works

--- src/service/cli/fill/content.js
+++ src/service/cli/fill/content.js
@@ -11,31 +11,31 @@ module.exports = ({
   users,
 }) =>
   `ALTER TABLE categories DISABLE TRIGGER ALL;
-INSERT INTO categories VALUES
+INSERT INTO categories (name) VALUES
 ${getSQLStringFromArray(categories)};
 ALTER TABLE categories ENABLE TRIGGER ALL;
 
 ALTER TABLE users DISABLE TRIGGER ALL;
-INSERT INTO users VALUES
+INSERT INTO users (avatar, firstName, lastName, email, passwordHash) VALUES
 ${getSQLStringFromArray(users)};
 ALTER TABLE users ENABLE TRIGGER ALL;
 
 ALTER TABLE articles DISABLE TRIGGER ALL;
-INSERT INTO articles VALUES
+INSERT INTO articles (title, createdAt, announce, fullText, userId) VALUES
 ${getSQLStringFromArray(notes)};
 ALTER TABLE articles ENABLE TRIGGER ALL;
 
 ALTER TABLE photos DISABLE TRIGGER ALL;
-INSERT INTO photos VALUES
+INSERT INTO photos (name, uniqueName, articleId) VALUES
 ${getSQLStringFromArray(photos)};
 ALTER TABLE photos ENABLE TRIGGER ALL;
 
 ALTER TABLE comments DISABLE TRIGGER ALL;
-INSERT INTO comments VALUES
+INSERT INTO comments (text, createdAt, userId, articleId) VALUES
 ${getSQLStringFromArray(comments)};
 ALTER TABLE comments ENABLE TRIGGER ALL;
 
 ALTER TABLE articles_categories DISABLE TRIGGER ALL;
-INSERT INTO articles_categories VALUES
+INSERT INTO articles_categories (articleId, categoryId) VALUES
 ${getSQLStringFromArray(notesCategories)};
 ALTER TABLE articles_categories ENABLE TRIGGER ALL;`;
